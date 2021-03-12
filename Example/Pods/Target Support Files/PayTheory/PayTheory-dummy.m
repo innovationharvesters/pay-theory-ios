5afe917cc9233d9d33a6255840ede131c313e3de
Austin Zani <austinzani@gmail.com>
2021-03-12 11:19:56 -0500

    removed useless files

--- Example/Pods/Target Support Files/PayTheory/PayTheory-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_PayTheory : NSObject
-@end
-@implementation PodsDummy_PayTheory
-@end
--- Example/Pods/Target Support Files/Pods-PayTheory_Tests/Pods-PayTheory_Tests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_PayTheory_Tests : NSObject
-@end
-@implementation PodsDummy_Pods_PayTheory_Tests
-@end
